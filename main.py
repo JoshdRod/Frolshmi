f1a5bc1b7a9f83f660c0bec0886ed92dac8ee091
Joshua Rodriguez <joshrod1089@gmail.com>
2024-07-05 13:51:44 +0100

    fixed SigmaWeight to multiply each input by its respective weight, instead of multiplying one input by every weight, and taking that as the weighted sum

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import numpy as np
 from PIL import Image
 
 # Freddie ;)
+"""
+Converts a CSV image file into an array of pixel values
+"""
 def CsvToArray():
   sigmalabels = []
   data = []
@@ -23,36 +26,50 @@ def InitialiseParams():
   pass
 
 # TEmi
-def RELU():
-  pass
+def RELU(x):
+  return x if x > 0 else 0
 
 # TEmI
 def SoftMax(weightsum):
   e_weightsum = np.exp(weightsum - np.max(weightsum))
   return e_weightsum / e_weightsum.sum()
 
-scores = [-0.3, 0.5, 7, 1.2]
-print(SoftMax(scores))
+#scores = [-0.3, 0.5, 7, 1.2]
+#print(SoftMax(scores))
 
 # Ollie
-def SigmaWeight(fredinfoalpha, fredinfolots):
+"""
+Performs the weighted sum operation for a whole layer, simeltaneously
+INPUTS: Input array, 2d array of weights for each ouput node [[weights for node 0], [weights for node 1], ..]
+RETURNS: array of the weighted sum for each node in next layer [sum for node 0, sum for node 2, ..]
+"""
+def SigmaWeight(fredinputsalpha, fredweightsalpha):
   fredtemp = 0
-  fredfinal = []
+  fredfinal = [] # Sums at each node (size = number of nodes in next layer)
   fredconstant1 = 0
-  for fred in range(0, 9):
-    for fred2 in range(0, 783):
-      for fred3 in range(0, 783):
-        fredtemp = fredtemp + fredinfoalpha[fred2]*fredinfolots[fred][fred3]
-      fredconstant1 = fredconstant1 + fredtemp
-      fredtemp = 0
+  # For each node in the layer:
+  for fred in range(10):
+    # For each input in the previous layer:
+    for fred2 in range(784):
+      # Multiply an input by all its weights, and add them together
+      fredtemp = fredtemp + fredinputsalpha[fred2]*fredweightsalpha[fred][fred2] # Weighted sum at node N = I1*w1 + I2*w2 + ... + In*wn
+    fredconstant1 = fredconstant1 + fredtemp
+    fredtemp = 0
+    # Add the total sum at that node to array
     fredfinal.append(fredconstant1)
     fredconstant1 = 0
   return fredfinal
-  
-  
-  
+   
 def ForwardPropagation():
   pass
 
 def Train():
   pass
+
+
+def main():
+  x = SigmaWeight([3]*784, [[2]*784]*10)
+  print(len(x))
+  print(x)
+
+main()
