6eeab47b837375c5d9b25b22e9306b54c83400f1
Fred Ayres <165328416+FredA2007@users.noreply.github.com>
2024-07-11 22:45:56 +0100

    hellooo

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import math
 import numpy as np
 from numpy import asarray
 from PIL import Image
+import random,csv
 
 # Freddie ;)
 """
@@ -9,14 +10,18 @@ Converts a CSV of image pixels into an array of pixel values
 INPUTS: CSV File name
 RETURNS: 2d array of all the pixel values for each image [[pixels in img 1], [pixels in img 2], ..]
 """
-def CsvToArray():
-  sigmalabels = []
+def CsvToArray(filename="train.csv"):
   data = []
-  with open("train.csv", "r") as file: # Might be good to make the file name a param, and default it to the "train.csv" (I belive there's a programming principle which involves this idea, but I can't remember it!)
-    training_data = file.reader(file)
-    for row in data:
-      sigmalabels.append(row[0])
-      data.append(row[1:])
+  with open(filename, "r") as file: # Might be good to make the file name a param, and default it to the "train.csv" (I belive there's a programming principle which involves this idea, but I can't remember it!)
+    training_data = csv.reader(file)
+    counter = 0 
+    for row in training_data:
+      if counter == 0:
+        sigmalabels = row
+      else:
+        int_row = [int(i) for i in row]
+        data.append(int_row)
+      counter += 1
   return sigmalabels, data
 
 """
@@ -73,19 +78,49 @@ def SigmaWeight(fredinputsalpha, fredweightsalpha):
     fredfinal.append(fredconstant1)
     fredconstant1 = 0
   return fredfinal
-   
-def ForwardPropagation():
-  pass
 
+"""
+Creates a n*m list of random nums betweeen 0 and 1, to be used as  weights or a whole layer.
+Used on first forward prop, to initialise weights ready for training
+INPUTS: int number of nodes in previous layer (tells us how many weights there should be going into each node), n
+        int number of nodes in current layer (tells us how many sets of weights we're going to need), m
+RETURNS: n*m list of random nums 0 <-> 1
+"""
+def getRandomWeights(noPreviousLayerNodes: int, noCurrentLayerNodes: int) -> list:
+  return [[random.random() for i in range(noPreviousLayerNodes)] for i in range(noCurrentLayerNodes)] # What's the advantage of using a numpy array here? I feel like there should be one
+
+"""
+Performs singular forward pass through network
+INPUTS: list input pixels, list hidden layer weights, list output layer weights
+RETURNS: int predicted number
+Also stores all values at each step in the network, to be passed into backpropogation
+""" 
+def ForwardPropagation(input: list, hiddenWeights=getRandomWeights(784, 10), outputWeights=getRandomWeights(10, 784)):
+  
+  hiddenWeightedSums = SigmaWeight(input, hiddenWeights)
+  hiddenActivations = map(RELU, hiddenWeightedSums)
+  
+  samples = [(random.randint(0, 10), random.randint(0, 783)) for i in range(3)]
+  for sample in samples:
+    print(f"Input : {input[sample[0]][sample[1]]}")
+    print(f"Weight : {hiddenWeights[sample[0]][sample[1]]}")
+    print(f"Weighted Sum : {hiddenWeightedSums[sample[0]]}")
+    print(f"Activation : {hiddenActivations[sample]}")
+  """
+  ouputWeightedSum
+  ouputActivation
+
+  prediction
+  """
 
 def Train():
   pass
 
 
-def main():
-  x = SigmaWeight([3]*784, [[2]*784]*10)
-  print(len(x))
-  print(x)
+#def main():
+  #ForwardPropagation()
 
 #main()
-pngToArray("colours.png")
+#pngToArray("colours.png")
+label, data = CsvToArray("mnist_train.csv")
+print(label)
